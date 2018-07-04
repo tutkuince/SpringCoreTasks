14b686a79a5a72734bae54c0e2e51ff7b018bfe1
tutkuince <tutku.ince@outlook.com>
2018-07-04 14:19:16 +0300

    created Sequence class

---
+++ src/io/spring/part01/model/Sequence.java
@@ -0,0 +1,27 @@
+package io.spring.part01.model;
+
+public class Sequence {
+	private final String id;
+	private final String prefix;
+	private final String suffix;
+
+	public Sequence(String id, String prefix, String suffix) {
+		super();
+		this.id = id;
+		this.prefix = prefix;
+		this.suffix = suffix;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+}
