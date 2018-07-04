bcc4d70358910febc19a12c1dff37803d2f8af95
tutkuince <tutku.ince@outlook.com>
2018-07-04 14:37:22 +0300

    modified

--- src/io/spring/part01/Main.java
+++ src/io/spring/part01/Main.java
@@ -2,17 +2,26 @@ package io.spring.part01;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import io.spring.part01.config.SequenceGeneratorConfiguration;
+//import io.spring.part01.config.SequenceGeneratorConfiguration;
+import io.spring.part01.dao.SequenceDao;
 
 public class Main {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-				SequenceGeneratorConfiguration.class);
-
-		SequenceGenerator generator = context.getBean("sequenceGenerator", SequenceGenerator.class);
-
-		System.out.println(generator.getSequence());
-		System.out.println(generator.getSequence());
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+//				SequenceGeneratorConfiguration.class);
+//
+//		SequenceGenerator generator = context.getBean("sequenceGenerator", SequenceGenerator.class);
+//
+//		System.out.println(generator.getSequence());
+//		System.out.println(generator.getSequence());
+//		
+//		context.close();
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("io.spring.part01");
+		SequenceDao sequenceDao = context.getBean(SequenceDao.class);
+		
+		System.out.println(sequenceDao.getNextValue("IT"));
+		System.out.println(sequenceDao.getNextValue("IT"));
 		
 		context.close();
 	}
