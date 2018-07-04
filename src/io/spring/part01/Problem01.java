e87dff814f2336e717daeb7f5d833945e834bbdf
tutkuince <tutku.ince@outlook.com>
2018-07-04 13:39:05 +0300

    created Problem01 class for definining first problem

---
+++ src/io/spring/part01/Problem01.java
@@ -0,0 +1,20 @@
+package io.spring.part01;
+
+public class Problem01 {
+	/**
+	 * Problem
+	 * 
+	 * You want to manage POJOs with annotations with Spring’s IoC container.
+	 * 
+	 * Solution
+	 * 
+	 * Design a POJO class. Next, create a Java config class with @Configuration
+	 * and @Bean annotations to configure POJO instance values or set up Java
+	 * components with @Component, @Repository, @Service, or
+	 * 
+	 * @Controller annotations to later create POJO instance values. Next,
+	 *             instantiate the Spring IoC container to scan for Java classes
+	 *             with annotations. The POJO instances or bean instances then
+	 *             become accessible to put together as part of an application.
+	 */
+}
