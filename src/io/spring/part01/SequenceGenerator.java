386e98ba3b7df2da1e1a151c6b27ed396f13fd16
tutkuince <tutku.ince@outlook.com>
2018-07-04 14:14:29 +0300

    modified class. added new field that is id and remove counter filed. And added new constructor

--- src/io/spring/part01/SequenceGenerator.java
+++ src/io/spring/part01/SequenceGenerator.java
@@ -1,35 +1,27 @@
 package io.spring.part01;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class SequenceGenerator {
+	private int id;
 	private String prefix;
 	private String suffix;
-	private int initial;
 
-	// store the numeric value of each generator
-	private final AtomicInteger counter = new AtomicInteger();
-
-	public SequenceGenerator() {
-
-	}
-
-	public void setPrefix(String prefix) {
+	public SequenceGenerator(int id, String prefix, String suffix) {
+		super();
+		this.id = id;
 		this.prefix = prefix;
-	}
-
-	public void setSuffix(String suffix) {
 		this.suffix = suffix;
 	}
 
-	public void setInitial(int initial) {
-		this.initial = initial;
+	public int getId() {
+		return id;
 	}
 
-	// get the last sequence number with the prefix and suffix joined.
-	public String getSequence() {
-		StringBuilder builder = new StringBuilder();
-		builder.append(prefix).append(initial).append(counter.getAndIncrement()).append(suffix);
-		return builder.toString();
+	public String getPrefix() {
+		return prefix;
 	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
 }
