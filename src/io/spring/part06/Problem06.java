fc6047947a6babdf7a432b6615ba3570da1218c5
tutkuince <tutku.ince@outlook.com>
2018-07-09 20:22:02 +0300

    Update definition

--- src/io/spring/part06/Problem06.java
+++ src/io/spring/part06/Problem06.java
@@ -1,5 +1,23 @@
 package io.spring.part06;
 
 public class Problem06 {
-
+	/**
+	 * Resolve I18N Text Messages for Different Locales in Properties Files
+	 * 
+	 * Problem
+	 * 
+	 * You want an application to support internationalization via annotations.
+	 * 
+	 * 
+	 * Solution
+	 * 
+	 * MessageSource is an interface that defines several methods for resolving
+	 * messages in resource bundles. ResourceBundleMessageSource is the most common
+	 * MessageSource implementation that resolves messages from resource bundles for
+	 * different locales. After you implement a ResourceBundleMessageSource POJO,
+	 * you can use the @Bean annotation in a Java config file to make the i18n data
+	 * available in an application.
+	 * 
+	 * 
+	 */
 }
