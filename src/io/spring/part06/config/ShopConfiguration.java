20d85a4b9b042248d23463e32c0e62f9b6192d30
tutkuince <tutku.ince@outlook.com>
2018-07-09 20:22:33 +0300

    created Config file

---
+++ src/io/spring/part06/config/ShopConfiguration.java
@@ -0,0 +1,25 @@
+package io.spring.part06.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+@ComponentScan(basePackages = "io.spring.part05")
+public class ShopConfiguration {
+
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:io/spring/part06/resources/messages");
+		messageSource.setCacheSeconds(1);
+		return messageSource;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
