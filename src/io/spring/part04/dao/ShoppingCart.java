2860bf6c40b9ac88066ae3d557a31da368d89308
tutkuince <tutku.ince@outlook.com>
2018-07-09 13:19:14 +0300

    appended new annotation(@Scope) for request type

--- src/io/spring/part04/dao/ShoppingCart.java
+++ src/io/spring/part04/dao/ShoppingCart.java
@@ -3,11 +3,13 @@ package io.spring.part04.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import io.spring.part04.model.Product;
 
 @Component
+@Scope("prototype")	// Creates a new bean instance each time when requested
 public class ShoppingCart {
 	private List<Product> items = new ArrayList<>();
 
