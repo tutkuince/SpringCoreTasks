fb67572ca9b9f7b4e07616e8c4476ad9c0f72075
tutkuince <tutku.ince@outlook.com>
2018-07-09 13:01:26 +0300

    update definition

--- src/io/spring/part04/Problem04.java
+++ src/io/spring/part04/Problem04.java
@@ -4,27 +4,19 @@ public class Problem04 {
 	/**
 	 * Problem
 	 * 
-	 * You want to use the Java standard @Resource and @Inject annotations to
-	 * reference POJOs via autowiring, instead of using the
-	 * Spring-specific @Autowired annotation.
+	 * When you declare a POJO instance with an annotation like @Component, you are
+	 * actually defining a template for bean creation, not an actual bean instance.
+	 * When a bean is requested by the getBean() method or referenced from other
+	 * beans, Spring decides which bean instance should be returned according to the
+	 * bean scope. Sometimes you have to set an appropriate scope for a bean other
+	 * than the default scope.
 	 * 
 	 * Solution
 	 * 
-	 * JSR-250, or Common Annotations for the Java Platform, defines the @Resource
-	 * annotation to autowire POJO references by name. JSR-330, or Standard
-	 * Annotations for Injection, defines the @Inject annotations to autowire POJO
-	 * references by type.
-	 * 
-	 * How It Works
-	 * 
-	 * The @Autowired annotation described in the previous recipe belongs to the
-	 * Spring Framework, specifically to the
-	 * org.springframework.beans.factory.annotation package. This means it can be
-	 * used only in the context of the Spring Framework. Soon after Spring added
-	 * support for the @Autowired annotation, the Java language standardized various
-	 * annotations to fulfill the same purpose of the @Autowired annotation. These
-	 * annotations are @Resource, which belongs to the javax.annotation package,
-	 * and @Inject, which belongs to the javax.inject package.
-	 * 
+	 * A bean’s scope is set with the @Scope annotation. By default, Spring creates
+	 * exactly one instance for each bean declared in the IoC container, and this
+	 * instance is shared in the scope of the entire IoC container. This unique bean
+	 * instance is returned for all subsequent getBean() calls and bean references.
+	 * This scope is called singleton, which is the default scope of all beans.
 	 */
 }
