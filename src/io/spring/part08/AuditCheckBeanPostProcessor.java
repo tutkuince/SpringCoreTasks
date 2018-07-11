03e16bf200cadd6341e61b3b18ffa48e969f3e7e
tutkuince <tutku.ince@outlook.com>
2018-07-11 14:41:19 +0300

    sample about post processor

---
+++ src/io/spring/part08/AuditCheckBeanPostProcessor.java
@@ -0,0 +1,18 @@
+package io.spring.part08;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class AuditCheckBeanPostProcessor implements BeanPostProcessor {
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In AuditCheckBeanPostProcessor.postProcessAfterInitialization, processing bean type: "
+				+ bean.getClass());
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		return bean;
+	}
+}
