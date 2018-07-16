1dd7440133cb0355806100ec359de8585b555f83
tutkuince <tutku.ince@outlook.com>
2018-07-16 11:44:34 +0300

    injecting ProductCreater

--- src/io/spring/part09/ShopConfiguration.java
+++ src/io/spring/part09/ShopConfiguration.java
@@ -1,12 +1,28 @@
 package io.spring.part09;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.spring.part07.model.Battery;
+import io.spring.part07.model.Disc;
 import io.spring.part07.model.Product;
 
 @Configuration
 public class ShopConfiguration {
+
+	@Bean	// instance factory method
+	public ProductCreator productCreatorFactory() {
+		ProductCreator factory = new ProductCreator();
+		Map<String, Product> products = new HashMap<>();
+		products.put("aaa", new Battery("AAA", 2.5, 0));
+		products.put("cdrw", new Disc("CD-EW", 1.5, 0));
+		products.put("dvdrw", new Disc("DVD-RW", 3.0, 0));
+		factory.setProducts(products);
+		return factory;
+	}
 
 	@Bean
 	public Product aaa() {
