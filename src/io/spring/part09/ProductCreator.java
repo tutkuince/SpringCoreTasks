e7f216c2a8ae909fb56a98e2e55bc5e03ffeac39
tutkuince <tutku.ince@outlook.com>
2018-07-16 11:44:05 +0300

    added instance factory method and new field(products)

--- src/io/spring/part09/ProductCreator.java
+++ src/io/spring/part09/ProductCreator.java
@@ -1,11 +1,18 @@
 package io.spring.part09;
 
+import java.util.Map;
+
 import io.spring.part07.model.Battery;
 import io.spring.part07.model.Disc;
 import io.spring.part07.model.Product;
 
 public class ProductCreator {
-	
+	private Map<String, Product> products;
+
+	public void setProducts(Map<String, Product> products) {
+		this.products = products;
+	}
+
 	public static Product createProduct(String productId) {
 		if ("aaa".equals(productId))
 			return new Battery("AAA", 2.5, 0);
@@ -13,6 +20,14 @@ public class ProductCreator {
 			return new Disc("CD-RW", 1.5, 0);
 		else if ("dvdrw".equals(productId))
 			return new Disc("DVD-RW", 3.0, 0);
+		throw new IllegalArgumentException("Unknown product");
+	}
+
+	public Product createInstanceProduct(String productId) {
+		Product product = products.get(productId);
+		if (product != null)
+			return product;
+
 		throw new IllegalArgumentException("Unknown product");
 	}
 }
